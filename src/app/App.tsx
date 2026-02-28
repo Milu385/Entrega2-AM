8fcf4e6eab1a476062843fdc57f755735481102c
santymaya9 <santymaya9@gmail.com>
2026-02-28 16:30:32 -0500

    Actualiza encabezado de App.tsx para el paso 17

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 16
+// Proceso Automático - Construcción del Prototipo - Iteración 17
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
