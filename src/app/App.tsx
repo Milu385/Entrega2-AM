e62aa4a83c5276a619b7f289833d1527f5ef1c3d
santymaya9 <santymaya9@gmail.com>
2026-02-28 16:28:14 -0500

    Actualiza encabezado de App.tsx para el paso 14

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 13
+// Proceso Automático - Construcción del Prototipo - Iteración 14
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
