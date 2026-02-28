f7a3ec512c1b7e7f1bd782246c88f0bc545f5d75
Z3rks <santiago.p1290@gmail.com>
2026-02-28 18:54:34 -0500

    correcion del swipe derecha izquierda

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 25
+// Proceso Automático - Construcción del Prototipo - Iteración 26
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
