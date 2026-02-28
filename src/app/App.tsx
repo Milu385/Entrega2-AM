f8cf205155c84c8635931efa73ab0bf7ee25024b
Z3rks <santiago.p1290@gmail.com>
2026-02-28 18:48:20 -0500

    detalles de la vista para acceder a la galeria de imagenes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 23
+// Proceso Automático - Construcción del Prototipo - Iteración 24
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
