79fff98a6aacfafa28c6cba3f1cad65875fc5ed4
Z3rks <santiago.p1290@gmail.com>
2026-02-28 19:58:25 -0500

    correcion panel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 30
+// Proceso Automático - Construcción del Prototipo - Iteración 34
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
