916b2c2b051e8fdc8a5d23e6fadae52f182dfd47
Z3rks <santiago.p1290@gmail.com>
2026-02-28 20:28:23 -0500

    login autentica favoritos y perfil

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 36
+// Proceso Automático - Construcción del Prototipo - Iteración 39
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
