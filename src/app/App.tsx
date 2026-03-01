7bc672ee3689b1e3066f390554a2bfb6fee7800f
Z3rks <santiago.p1290@gmail.com>
2026-02-28 20:26:12 -0500

    notificacion

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 35
+// Proceso Automático - Construcción del Prototipo - Iteración 36
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
