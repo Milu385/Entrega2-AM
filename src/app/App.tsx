1c7a9a4de506bad169c950d4cbcf9a07015b5706
Z3rks <santiago.p1290@gmail.com>
2026-02-28 19:18:09 -0500

    ya si da el swipe

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-// Proceso Automático - Construcción del Prototipo - Iteración 27
+// Proceso Automático - Construcción del Prototipo - Iteración 29
 import { useState } from "react";
 import { BottomNav } from "./components/BottomNav";
 import { PropertyCard } from "./components/PropertyCard";
