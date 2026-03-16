52777d1b56c8b7c42446be3f2122340ab973c0d3
milu385 <danicgrt@gmail.com>
2026-03-16 07:21:04 -0500

    terminos y condiciones

--- android/app/src/main/java/com/mihogar/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.mihogar.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mihogar.app',
-  appName: 'Mi Hogar',
-  webDir: 'dist'
-};
-
-export default config;
